18f88d43ac5aa5f63021150baa2f915d7bea45ab
sakriwassim <sakriwassim@gmail.com>
2024-05-19 23:41:19 +0100

    add the icon and change the name of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hospital_car/signin_screen.dart';
-import 'package:hospital_car/splash_page.dart';
 import 'firebase_options.dart';
 
 Future main()  async {
@@ -25,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: const SigninPage(),
     );
   }
 }
